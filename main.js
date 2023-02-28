fbf511c059e379ea0a8bcc4961e0055e5b63d7e4
paulcarter14 <paulcarter190410@gmail.com>
2023-02-28 13:51:27 +0100

    better

--- main.js
+++ main.js
@@ -12,73 +12,73 @@ const itemsCounter = document.querySelector('#items-counter');
 //testade att lägga detta i en function som kallades på.
 //funkar inte men tror det kan bero på handhavandefel
 function hideButtons() {
-    showbuttons.classList.add("hidden")
-    checkAll.classList.add("hidden")
+  showbuttons.classList.add("hidden")
+  checkAll.classList.add("hidden")
 }
- //samma här då de alltid syns efter första tasken lagts till.
+//samma här då de alltid syns efter första tasken lagts till.
 function showButtons() {
-    showbuttons.classList.remove("hidden")
-    checkAll.classList.remove("hidden")
+  showbuttons.classList.remove("hidden")
+  checkAll.classList.remove("hidden")
 }
 
-function taskAmount () {
-    let amount = list.childElementCount;
-    document.getElementById("amount").innerHTML = amount;
-  }
-  
+function taskAmount() {
+  let amount = list.childElementCount;
+  document.getElementById("amount").innerHTML = amount;
+}
+
+
 hideButtons();
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const inputValue = input.value;
-    if (inputValue === '') {
-        alert('Skriv något!');
-        return;
+  event.preventDefault();
+  const inputValue = input.value;
+  if (inputValue === '') {
+    alert('Skriv något!');
+    return;
+  }
+  const listItem = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  const taskText = document.createTextNode(inputValue);
+  const deleteButton = document.createElement('input');
+  deleteButton.type = 'button';
+  deleteButton.value = '❌';
+  deleteButton.addEventListener('click', function () {
+    listItem.remove();
+    if (list.childElementCount === 0) {
+      hideButtons();
     }
-    const listItem = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    const taskText = document.createTextNode(inputValue);
-    const deleteButton = document.createElement('input');
-    deleteButton.type = 'button';
-    deleteButton.value = '❌';
-    deleteButton.addEventListener('click', function () {
-        listItem.remove();
-        if (list.childElementCount === 0) {
-            hideButtons();
-        }
     taskAmount();
 
-        
-    });
-    listItem.appendChild(checkbox);
-    listItem.appendChild(taskText);
-    listItem.appendChild(deleteButton);
-    list.appendChild(listItem);
-    input.value = '';
-    showButtons();
-   
-    taskAmount();
+
+  });
+  listItem.appendChild(checkbox);
+  listItem.appendChild(taskText);
+  listItem.appendChild(deleteButton);
+  list.appendChild(listItem);
+  input.value = '';
+  showButtons();
+  taskAmount();
 });
 
 checkAll.addEventListener('click', function (event) {
-    event.preventDefault();
-    let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    let counter = 0;
+  event.preventDefault();
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  let counter = 0;
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      counter++;
+    }
+  }
+  if (counter === checkboxes.length) {
     for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked === true) {
-            counter++;
-        }
+      checkboxes[i].checked = false;
     }
-    if (counter === checkboxes.length) {
-        for (let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = false;
-        }
-    } else {
-        for (let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = true;
-        }
+  } else {
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = true;
     }
+  }
 });
 
 
@@ -89,52 +89,52 @@ checkAll.addEventListener('click', function (event) {
 //samt den.
 
 showAll.addEventListener('click', function (event) {
-    event.preventDefault();
-    let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].parentElement.style.display = 'block';
-    }
+  event.preventDefault();
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].parentElement.style.display = 'block';
+  }
 });
 
 showActive.addEventListener('click', function (event) {
-    event.preventDefault();
-    let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked === false) {
-            checkboxes[i].parentElement.style.display = 'block';
-        } else {
-            checkboxes[i].parentElement.style.display = 'none';
-        }
+  event.preventDefault();
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === false) {
+      checkboxes[i].parentElement.style.display = 'block';
+    } else {
+      checkboxes[i].parentElement.style.display = 'none';
     }
+  }
 });
 
 showCompleted.addEventListener('click', function (event) {
-    event.preventDefault();
-    let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked === true) {
-            checkboxes[i].parentElement.style.display = 'block';
-        } else {
-            checkboxes[i].parentElement.style.display = 'none';
-        }
+  event.preventDefault();
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      checkboxes[i].parentElement.style.display = 'block';
+    } else {
+      checkboxes[i].parentElement.style.display = 'none';
     }
+  }
 });
 
 
 //Har bråkat med denna. Den vill inte fungera,
 //knappen syns inte och jag fattar fan noll.
 clearCompleted.addEventListener('click', function (event) {
-    event.preventDefault();
-    let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    let tasksLeft = checkboxes.length;
-    for (let i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked === true) {
-        checkboxes[i].parentElement.remove();
-      }
+  event.preventDefault();
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  let tasksLeft = checkboxes.length;
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      checkboxes[i].parentElement.remove();
     }
-    if(list.childElementCount === 0){
-        hideButtons();
-    }
-    taskAmount();
+  }
+  if (list.childElementCount === 0) {
+    hideButtons();
+  }
+  taskAmount();
 
-  });
+});
